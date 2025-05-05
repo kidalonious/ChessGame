ac4af5bc560aeff7fad47d998b02a1edfbdaa9dd
kidalonious <120538119+kidalonious@users.noreply.github.com>
2025-05-04 22:25:03 -0600

    overrode quals and hashcode for position class

--- ChessGame.Core/Position.cs
+++ ChessGame.Core/Position.cs
@@ -9,5 +9,30 @@ namespace ChessGame.Core {
             this.rank = rank;
             this.file = file;
         }
+        override // override object.Equals
+        public override bool Equals(object obj)
+        {
+            //
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+            
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            
+            // TODO: write your implementation of Equals() here
+            return rank == obj.rank && file == obj.file
+        }
+        
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            // TODO: write your implementation of GetHashCode() here
+            return HashCode.Combine(rank, file);
+        }
     }
 }
